9ba6e3df5f945834666c578f678c6494523882e2
Henrique Tavares <ihenrits@gmail.com>
2023-02-03 16:44:25 -0300

    reconfiguring project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
+import { useEffect } from 'react'
+
+import { axeAccessibilityReporter } from '@/utils/axeAccessibilityReporter'
+
 import type { AppProps } from 'next/app'
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    axeAccessibilityReporter()
+  }, [])
+  
   return <Component {...pageProps} />
 }
